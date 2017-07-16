937ed302968620d58c6a2b05c2081611c5061650
Adrien Castex <adrien.castex@protonmail.com>
2017-07-16 19:11:14 +0200

    Now, the 'Stream' interface is almost an alias of 'request.Request'

--- lib/request.d.ts
+++ lib/request.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
-/// <reference types="request" />
 import * as req from 'request';
+import * as http from 'http';
 export declare type ContentType = Buffer | string;
 export interface RequestOptions {
     url: string;
@@ -19,11 +19,13 @@ export interface Response {
 }
 export declare type ResponseCallback = (error: Error, response?: Response, body?: ContentType) => void;
 export declare function request(options: RequestOptions, callback: ResponseCallback): void;
-export declare class Stream {
-    protected stream: req.Request;
-    constructor(stream: req.Request);
-    on(event: 'error' | 'data' | 'end' | 'finish' | 'complete', callback: Function): void;
-    write(data: ContentType, callback?: (error: Error) => void): void;
-    end(data: ContentType, callback?: (error: Error) => void): void;
+export interface Stream extends req.Request {
+    on(event: string, listener: Function): this;
+    on(event: 'request', listener: (req: http.ClientRequest) => void): this;
+    on(event: 'response' | 'finish', listener: (resp: http.IncomingMessage) => void): this;
+    on(event: 'data', listener: (data: Buffer | string) => void): this;
+    on(event: 'error', listener: (e: Error) => void): this;
+    on(event: 'complete', listener: (resp: http.IncomingMessage, body?: string | Buffer) => void): this;
+    on(event: 'end', listener: () => void): this;
 }
 export declare function stream(options: RequestOptions): Stream;
--- src/request.ts
+++ src/request.ts
@@ -1,5 +1,6 @@
 import { Readable, Writable } from 'stream'
 import * as req from 'request'
+import * as http from 'http'
 
 export type ContentType = Buffer | string;
 
@@ -35,40 +36,25 @@ export function request(options : RequestOptions, callback : ResponseCallback) :
     })
 }
 
-export class Stream
+export interface Stream extends req.Request
 {
-    constructor(protected stream : req.Request)
-    { }
-
-    on(event : 'error' | 'data' | 'end' | 'finish' | 'complete', callback : Function)
-    {
-        switch(event)
-        {
-            case 'finish':
-                this.stream.on('response', callback as (arg) => void);
-                break;
-            
-            case 'end':
-                this.stream.on('complete', callback as () => void);
-                break;
-            
-            default:
-                this.stream.on(event, callback as () => void);
-                break;
-        }
-    }
-
-    write(data : ContentType, callback ?: (error : Error) => void)
-    {
-        this.stream.write(data as any, callback);
-    }
-    end(data : ContentType, callback ?: (error : Error) => void)
-    {
-        this.stream.end(data as any, callback);
-    }
+    on(event: string, listener: Function): this;
+    on(event: 'request', listener: (req: http.ClientRequest) => void): this;
+    on(event: 'response' | 'finish', listener: (resp: http.IncomingMessage) => void): this;
+    on(event: 'data', listener: (data: Buffer | string) => void): this;
+    on(event: 'error', listener: (e: Error) => void): this;
+    on(event: 'complete', listener: (resp: http.IncomingMessage, body?: string | Buffer) => void): this;
+    on(event: 'end', listener: () => void): this;
 }
+
+//export type Stream = req.Request
 
 export function stream(options : RequestOptions) : Stream
 {
-    return new Stream(req(options));
+    const stream = req(options);
+    stream.on('response', (resp) => {
+        stream.emit('finish', resp);
+    })
+    
+    return stream;
 }
